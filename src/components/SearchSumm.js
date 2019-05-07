5a592df63686f1b0e1b005e9a43d68733837e131
asukachikaru <asukachikaru@gmail.com>
2019-05-07 23:14:41 +0900

    [Misc] Style changes

--- src/components/SearchSumm.js
+++ src/components/SearchSumm.js
@@ -4,9 +4,21 @@ import '../style/SearchSumm.css';
 
 export default class SearchSumm extends Component {
   render() {
+    let str = '';
+    switch (this.props.numOfResults) {
+      case 0:
+        str = `No result for "${this.props.keyword}"`
+        break;
+      case 1: 
+        str = `1 result for "${this.props.keyword}"`
+        break;   
+      default:
+        str = `${this.props.numOfResults} results for "${this.props.keyword}"`
+        break;
+    }
     return (
       <h3 className="searchSumm" >
-        {this.props.numOfResults} results for "{this.props.keyword}"
+        {str}
       </h3>
     )
   }
