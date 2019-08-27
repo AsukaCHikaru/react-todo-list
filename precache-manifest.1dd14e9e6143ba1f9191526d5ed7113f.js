51e56d77e239991de6a240e01ff26fd662598b0e
Asuka Wang <asukachikaru@gmail.com>
2019-08-27 10:28:31 +0900

    Updates

---
+++ precache-manifest.1dd14e9e6143ba1f9191526d5ed7113f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e31a313ddba1bb456926cd7e32aae9",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "89ac02847d436034c51e",
+    "url": "/react-todo-list/static/css/main.f6fc4338.chunk.css"
+  },
+  {
+    "revision": "b91d5dc60610cb91c6ee",
+    "url": "/react-todo-list/static/js/2.889b7284.chunk.js"
+  },
+  {
+    "revision": "89ac02847d436034c51e",
+    "url": "/react-todo-list/static/js/main.a59d331b.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
